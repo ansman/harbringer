279ec0a7cd67bc4f5c5b10088924f98c1ce78582
Nicklas Ansman <nicklas@ansman.se>
2026-06-11 21:28:37 -0400

    Fix JUnit 6 test JVM target

--- buildSrc/src/main/kotlin/library.gradle.kts
+++ buildSrc/src/main/kotlin/library.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.accessors.dm.LibrariesForLibs
+import org.gradle.api.attributes.java.TargetJvmVersion
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.HasConfigurableKotlinCompilerOptions
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -36,11 +37,23 @@ afterEvaluate {
 
 tasks.withType<KotlinJvmCompile>().configureEach {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_11
+        jvmTarget = if (name == "compileTestKotlin") JvmTarget.JVM_17 else JvmTarget.JVM_11
     }
 }
 
 pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+    tasks.named<JavaCompile>("compileTestJava") {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+    }
+
+    configurations.named("testCompileClasspath") {
+        attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
+    }
+    configurations.named("testRuntimeClasspath") {
+        attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
+    }
+
     dependencies {
         "testImplementation"(libs.assertk)
         "testImplementation"(platform(libs.junit.bom))
